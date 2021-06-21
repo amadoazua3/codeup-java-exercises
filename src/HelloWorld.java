bf89592e48caca52b2d7c91303a075c62eed906b
Amado Azua III <amado.azua3@gmail.com>
2021-06-21 17:43:44 -0500

    Syntax, Types, and Variables

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,43 @@
 public class HelloWorld {
 
     public static void main(String[] args){
-        System.out.println("Hello, world!");
+
+//        System.out.println("Hello, world!");
+//
+//        int myFavoriteNumber = 10;
+//        String myString = "3.14159";
+//        float myNumber = 3.14f;
+//
+//
+//
+//        System.out.println(myFavoriteNumber);
+//        System.out.println(myString);
+//        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        int three = (int) "three";
+
+//        int x = 4;
+//        x = x + 5;
+
+//        int x = 3;
+//        int y = 4;
+//        y = y * x;
+
+//        int x = 10;
+//        int y = 2;
+//        x = x / y;
+//        y = y - x;
+
+//        System.out.println(y);
+
     }
 
 }
---
+++ src/SyntaxTypeVariables.java
@@ -0,0 +1,14 @@
+public class SyntaxTypeVariables {
+
+    public static void main(String[] args){
+
+        byte myFirstNumber = 127;
+        byte mySecondNumber = 1;
+
+        System.out.println(myFirstNumber + mySecondNumber);
+
+
+
+    }
+
+}
