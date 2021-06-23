8ef0b0aacd59af8fc79c2f188e3e6c1902cc8c7d
Amado Azua III <amado.azua3@gmail.com>
2021-06-23 10:51:51 -0500

    Java mini exercise

--- src/ConsoleIO.java
+++ src/ConsoleIO.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.*;
 
 public class ConsoleIO {
 
@@ -31,19 +32,35 @@ public class ConsoleIO {
 //        int myNum = sc.nextInt();
 //        System.out.println(myNum);
 
-        while (sc.hasNext()){
-            String word = sc.nextLine();
+//        while (sc.hasNext()){
+//            String word = sc.nextLine();
+//
+//            if(word.equals("break")){
+//                break;
+//            }
+//            System.out.println(word);
+//        }
 
-            if(word.equals("break")){
-                break;
-            }
-            System.out.println(word);
+        //        MINI EXERCISE
+        // TODO: create three unique usernames (user1, user2, user3).
+        //  create a fourth user and set it equal to user input. (ask
+        //  the user to enter a username). if the username has already
+        //  been used by user1, user2, or user3, print "sorry this
+        //  username already exists.
+
+        String user1 = "Vash";
+        String user2 = "Noel";
+        String user3 = "Amado";
+
+        System.out.println("Please input a new username: ");
+
+        String userInput = sc.next();
+
+        if(userInput.equals(user1) || userInput.equals(user2) || userInput.equals(user3)){
+            System.out.println("Sorry this username already exists.");
+        }else {
+            System.out.println("You have a valid username.");
         }
-
-
-
-
-
 
     }
 
