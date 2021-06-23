9ca619b430873d21b87a57f9ea1c136105a03b85
Amado Azua III <amado.azua3@gmail.com>
2021-06-23 09:35:43 -0500

    Java class exercises

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -7,19 +7,21 @@ public class ConsoleExercises {
 //        double pi = 3.14159;
 //        System.out.printf("The value of pi is approximately %.2f.", pi);
 
-//        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
 //        System.out.println("Please input a int: ");
 //        int userInput = scanner.nextInt();
 
 //        System.out.println("You entered: " + userInput);
 
-//        System.out.println("Please enter 3 words:");
+        System.out.println("Please enter 3 words:");
+        String sentence = scanner.nextLine();
 //        String word1 = scanner.next();
 //        String word2 = scanner.next();
 //        String word3 = scanner.next();
-//
+
 //        System.out.println(word1 + " " + word2 + " " + word3);
+        System.out.println(sentence);
 
 //        System.out.println("Please enter a sentence: ");
 //        String sentence = scanner.next();
@@ -28,7 +30,7 @@ public class ConsoleExercises {
 //        System.out.println(sentence);
 
 
-        Scanner scanner = new Scanner(System.in);
+//        Scanner scanner = new Scanner(System.in);
 
 
         System.out.println("Please enter a number for length: ");
