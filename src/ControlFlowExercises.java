14ff48eb96ee2f6ab99360b679e8535f85e4ea4b
Amado Azua III <amado.azua3@gmail.com>
2021-06-24 21:07:00 -0500

    Control flow java exercises

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,5 +1,9 @@
+import java.util.*;
+
 public class ControlFlowExercises {
     public static void main(String[] args) {
+
+//        1.(A)
 
 //        int i = 5;
 //
@@ -9,14 +13,52 @@ public class ControlFlowExercises {
 //            i++;
 //        }
 
-        int num = 0;
+//        2.(A)
 
-        do{
+//        int num = 0;
+//
+//        do{
+//
+//            System.out.printf("num is: %d\n", num);
+//            num += 2;
+//
+//        }while(num <= 100);
 
-            System.out.printf("num is: %d\n", num);
-            num += 2;
+//        2.(B)
 
-        }while(num <= 100);
+//        int num = 100;
+//        do{
+//            System.out.printf("num is: %d\n", num);
+//            num -= 5;
+//
+//        }while(num >= -5);
+
+//        2.(C)
+
+/* Due to the range of int the following sequence produced is
+
+                    2
+                    4
+                    16
+                    256
+                    65536
+                    0
+And then it remains zero. Note that it never rises above 1000000.
+
+With a long, the number after 65536 would be 4294967296
+(which does not fit in an int, but does fit in a long), so it stops. */
+
+//        long num = 2;
+//
+//        do{
+//
+//            System.out.printf("num is: %d\n", num);
+//            num *= num;
+//
+//        }while(num < 1000000);
+
+
+
 
     }
 }
