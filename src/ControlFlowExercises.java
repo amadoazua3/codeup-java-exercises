ce05ee6ff7c17292377c069a536fff4f29316664
Amado Azua III <amado.azua3@gmail.com>
2021-06-24 20:11:40 -0500

    Control flow java exercises

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,24 +1,22 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
-        int i = 5;
-
-        while(i <= 15){
-
-            System.out.printf("i: %d\n", i);
-            i++;
-        }
-
-//        work in progress still not done
-//        do{
-//            int num = 0;
-//            num *= 2;
-//            System.out.printf("num is: %d\n", num);
-//            if(num < 100){
-//                break;
-//            }
+//        int i = 5;
 //
-//        }while(true);
+//        while(i <= 15){
+//
+//            System.out.printf("i: %d\n", i);
+//            i++;
+//        }
+
+        int num = 0;
+
+        do{
+
+            num += 2;
+            System.out.printf("num is: %d\n", num);
+
+        }while(num < 100);
 
     }
 }
