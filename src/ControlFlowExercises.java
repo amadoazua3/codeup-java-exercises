20ca4e51c1e454039ad5cf979c5041744d6979f8
Amado Azua III <amado.azua3@gmail.com>
2021-06-26 00:27:16 -0500

    Java control statements and loops exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -94,11 +94,26 @@ With a long, the number after 65536 would be 4294967296
 
 //        3. Table of powers
 
+        Scanner scanner = new Scanner(System.in);
 
-//
-//        for(){
-//
-//        }
+        System.out.println("What number would you like to go up to?");
+
+        int input = scanner.nextInt();
+
+        System.out.println("Here is your table!\n");
+        System.out.println("number | squared | cubed");
+        System.out.println("------ | ------- | -----");
+
+        for(int i = 1; i <= input; i++){
+
+            int squared = i * i;
+            int cubed = i * i * i;
+
+            System.out.println(i +"      | " + squared + "\t\t | " + cubed);
+        }
+
+        System.out.println("Would you like to continue? (y/n)");
+        String answer = scanner.next();
 
 
     }
