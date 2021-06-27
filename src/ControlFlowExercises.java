0dee7834ddc1a8c3148888e184340303e4d5647e
Amado Azua III <amado.azua3@gmail.com>
2021-06-27 16:55:44 -0500

    Java control statements and loops exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -94,35 +94,85 @@ With a long, the number after 65536 would be 4294967296
 
 //        3. Table of powers
 
-        Scanner scanner = new Scanner(System.in);
+//        Scanner scanner = new Scanner(System.in);
+//
+//        while(true) {
+//
+//            System.out.println("What number would you like to go up to?");
+//
+//            int input = scanner.nextInt();
+//
+//            System.out.println("Here is your table!\n");
+//            System.out.println("number | squared | cubed");
+//            System.out.println("------ | ------- | -----");
+//
+//            for (int i = 1; i <= input; i++) {
+//
+//                int squared = i * i;
+//                int cubed = i * i * i;
+//
+//                System.out.println(i + "      | " + squared + "\t\t | " + cubed);
+//            }
+//
+//            System.out.println("Would you like to continue? (y/n)");
+//            String answer = scanner.next().toLowerCase();
+//
+//            if(answer.equals("y")) {
+//                continue;
+//            } else {
+//                break;
+//            }
+//
+//        }
 
-        while(true) {
+//        4. Number to letter grades
 
-            System.out.println("What number would you like to go up to?");
+//        while(true) {
+//
+//            System.out.println("Please enter a numerical grade from 0 to 100: ");
 
-            int input = scanner.nextInt();
+//        We are using byte for this example because we are assuming users will only input valid
+//        numbers from 0-100 which fits inside of bytes range.
 
-            System.out.println("Here is your table!\n");
-            System.out.println("number | squared | cubed");
-            System.out.println("------ | ------- | -----");
+//            byte grade = scanner.nextByte();
+//
+//           if(grade >= 96) {
+//               System.out.println("A+");
+//           } else if(grade >= 92){
+//               System.out.println("A");
+//           } else if(grade >= 88){
+//               System.out.println("A-");
+//           } else if(grade >= 85){
+//               System.out.println("B+");
+//           } else if(grade >= 83){
+//               System.out.println("B");
+//           } else if(grade >= 80){
+//               System.out.println("B-");
+//           } else if(grade >= 75){
+//               System.out.println("C+");
+//           } else if(grade >= 71){
+//               System.out.println("C");
+//           } else if(grade >= 67){
+//               System.out.println("C-");
+//           } else if(grade >= 64){
+//               System.out.println("D+");
+//           } else if(grade >= 62){
+//               System.out.println("D");
+//           } else if(grade >= 60){
+//               System.out.println("D-");
+//           } else {
+//               System.out.println("F");
+//           }
+//
+//            System.out.println("Would you like to continue? (y/n)");
+//            String prompt = scanner.next().toLowerCase();
+//
+//            if(prompt.equals("y")){
+//                continue;
+//            } else {
+//                break;
+//            }
+//        }
 
-            for (int i = 1; i <= input; i++) {
-
-                int squared = i * i;
-                int cubed = i * i * i;
-
-                System.out.println(i + "      | " + squared + "\t\t | " + cubed);
-            }
-
-            System.out.println("Would you like to continue? (y/n)");
-            String answer = scanner.next().toLowerCase();
-
-            if(answer.equals("y")) {
-                continue;
-            } else {
-                break;
-            }
-
-        }
     }
 }
