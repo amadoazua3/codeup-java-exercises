c9ebf08d3d2c90b09e60bec8cdad75a29946c7a4
Amado Azua III <amado.azua3@gmail.com>
2021-06-29 16:11:57 -0500

    Java methods exercise

---
+++ src/HighLow.java
@@ -0,0 +1,64 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class HighLow {
+
+    public static void main(String[] args) {
+
+
+
+        int mysteryNumber = randomNumber();
+
+        int userNum = userNumber();
+
+
+        guess(mysteryNumber, userNum);
+
+    }
+
+
+    public static int randomNumber(){
+
+        Random rand = new Random();
+        int upperbound = 100;
+        // generate random values from 0-100
+
+        int rando = rand.nextInt(upperbound);
+
+        return rando;
+    }
+
+    public static int userNumber(){
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Please pick a number between 1 and 100: ");
+        int userInput = scanner.nextInt();
+
+        if(userInput > 100){
+            userNumber();
+        } else if(userInput < 0){
+            userNumber();
+        }
+            return userInput;
+
+    }
+
+    public static void guess(int mysteryNum, int userNum){
+
+        if(userNum > mysteryNum){
+            System.out.println("LOWER");
+            userNumber();
+        } else if(userNum < mysteryNum){
+            System.out.println("HIGHER");
+            userNumber();
+        } else if(userNum == mysteryNum){
+            System.out.println("GOOD GUESS!");
+        }
+
+
+    }
+
+
+
+}
