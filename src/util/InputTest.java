45653425e80725e74a385f19be41045114919e80
Amado Azua III <amado.azua3@gmail.com>
2021-07-01 17:22:57 -0500

    Java inheritance and polymorphism exercise

--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -12,9 +12,9 @@ public class InputTest {
 
 //        System.out.println(input.getDouble(1, 50));
 
-//        System.out.println(input.getInt(1,50));
+        System.out.println(input.getInt(1,50));
 
-        System.out.println(input.yesNo());
+//        System.out.println(input.yesNo());
 
 
 
