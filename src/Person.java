7358b496f755df973182c19b540dd62280c7c5b4
Amado Azua III <amado.azua3@gmail.com>
2021-06-30 20:20:06 -0500

    Java OOP exercise

--- src/Person.java
+++ src/Person.java
@@ -13,8 +13,24 @@ public class Person {
         Person person = new Person("Amado");
 
 //        person.setName("Jesse");
-        person.sayHello();
+//        person.sayHello();
 
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName()));
+//        System.out.println(person1 == person2);
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1 == person2);
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+//        person2.setName("Jane");
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
 
     }
 
@@ -25,16 +41,18 @@ public class Person {
     }
 
     public String getName(){
-//TODO: return the person's name
+    //TODO: return the person's name
+
         return this.name;
     }
 
     public void setName(String name){
-//TODO: change the name property to the passed value
+    //TODO: change the name property to the passed value
+
         this.name = name;
     }
     public void sayHello(){
-//TODO: print a message to the console using the person's name
+    //TODO: print a message to the console using the person's name
 
         System.out.println("Hello " + this.name + "!");
 
