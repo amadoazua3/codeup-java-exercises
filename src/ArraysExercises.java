e05fc6b7614214ac79666090455c393a8de0d502
Amado Azua III <amado.azua3@gmail.com>
2021-07-02 21:15:26 -0500

    Java Arrays exercise

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -3,8 +3,8 @@ import java.util.Arrays;
 public class ArraysExercises {
 
     public static void main(String[] args) {
-        int[] numbers = {1, 2, 3, 4, 5};
 
+        int[] numbers = {1, 2, 3, 4, 5};
 
         toString(numbers);
 
@@ -14,11 +14,35 @@ public class ArraysExercises {
 //            System.out.println(n);
 //        }
 
+     String[] Person = new String[3];
+     Person[0] = "Haruko";
+     Person[1] = "Naota";
+     Person[2] = "Ninamori";
+
+     String personElement = "Canti";
+
+     for(String n: Person){
+         System.out.println(n);
+     }
+
+      addPerson(Person, personElement);
+
+//        System.out.println("Array after adding " + personElement +
+//        Arrays.toString());
+
     }
 
     private static void toString(int[] numbers) {
 
         System.out.println(Arrays.toString(numbers));
+
+    }
+
+    static String addPerson(String[] person, String element){
+
+        // this doesnt work just a stopping point for now
+        return person + element;
+
     }
 
 }
