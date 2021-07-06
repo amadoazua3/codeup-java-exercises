905eb1b93ab4dadd5e6cbb2a4f85bbe6671c736c
Amado Azua III <amado.azua3@gmail.com>
2021-07-06 15:06:34 -0500

    Java interface and abstract classes exercise

--- src/shapes/Quadrilateral.java
+++ src/shapes/Quadrilateral.java
@@ -2,6 +2,27 @@ package shapes;
 
 abstract class Quadrilateral extends Shape implements Measurable{
 
+        private double length;
+        private double width;
+
+    public Quadrilateral(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public abstract double setLength();
+
+    public abstract double setWidth();
+
+
 
 
 }
