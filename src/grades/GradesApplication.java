676af1b7bb77710911e47e9c6eb0c7638eb4bd6d
Amado Azua III <amado.azua3@gmail.com>
2021-07-09 17:19:26 -0500

    Java collections exercise

---
+++ src/grades/GradesApplication.java
@@ -0,0 +1,13 @@
+package grades;
+
+import java.util.HashMap;
+
+public class GradesApplication {
+
+    public static void main(String[] args) {
+
+        HashMap<> students = new HashMap<>();
+    }
+
+
+}
