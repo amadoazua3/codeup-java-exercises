1454d66b6efc2b422231a394d346b1b6ad4ac45f
Amado Azua III <amado.azua3@gmail.com>
2021-07-09 11:27:10 -0500

    Java collections lecture

--- src/collections/CollectionsLecture.java
+++ src/collections/CollectionsLecture.java
@@ -6,11 +6,12 @@ public class CollectionsLecture {
 
     public static void main(String[] args) {
 
-        ArrayList<String> stringArr = new ArrayList<>();
+        ArrayList<String> stringArr = new ArrayList<>(), stringArrCopy = new ArrayList<>();
 
         // add first element
 
         stringArr.add("Amado");
+        stringArrCopy.add("Amado");
 
 //        System.out.println(stringArr);
         System.out.printf("Current ArrayList size: %s\n", stringArr.size());
@@ -19,20 +20,44 @@ public class CollectionsLecture {
 
 //        System.out.println(stringArr.hashCode());
         System.out.println(System.identityHashCode(stringArr));
+        System.out.println("\n");
 
         // add second element
 
         stringArr.add("Haruko");
+
+        // returns true on .equals when the copy and stringArr are compared
+        stringArrCopy.add("Haruko");
+
+        // returns false on .equals when the copy and stringArr are compared
+//        stringArrCopy.add("Canti");
         System.out.printf("Current ArrayList size: %s\n", stringArr.size());
         System.out.println(stringArr);
 //        System.out.println(stringArr.hashCode());
         System.out.println(System.identityHashCode(stringArr));
+        System.out.println("\n");
 
         // remove second element
 
-        stringArr.remove(1);
+//        stringArr.remove(1);
 //        System.out.println(stringArr.hashCode());
-        System.out.println(System.identityHashCode(stringArr));
+//        System.out.println(System.identityHashCode(stringArr));
+
+
+//        ArrayList<String> stringArrCopy = stringArr;
+
+
+        System.out.println("Array list copy: ");
+        System.out.println(stringArrCopy);
+        System.out.println(System.identityHashCode(stringArrCopy));
+        System.out.println("\n");
+
+//        stringArrCopy.remove(1);
+//        System.out.println(stringArr);
+
+        System.out.println("The array lists are equal: " + stringArr.equals(stringArrCopy));
+
+
 
     }
 
