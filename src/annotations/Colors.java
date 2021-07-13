2329318a757cf07dc3b60a35515d55c5dc43f480
Amado Azua III <amado.azua3@gmail.com>
2021-07-13 10:57:15 -0500

    Annotations lecture

--- src/annotations/Colors.java
+++ src/annotations/Colors.java
@@ -2,13 +2,21 @@ package annotations;
 
 
 class Red {
-    public void show(){
+    public void showMyFavoriteColorOfAllTimefromTheRainbow(){
         System.out.println("I am red");
     }
+
+
+
 }
 
 class Orange extends Red {
-    public void show(){
+    @Override // comments for the compiler // it checks to see if that
+    // comment is doing what it's supposed to do
+
+    // hey this method should be overriding the method in it's parrant
+    // class of red
+    public void showMyFavoriteColorOfAllTimefromTheRainbow(){
         System.out.println("I am orange");
     }
 }
@@ -20,8 +28,8 @@ public class Colors {
     public static void main(String[] args) {
         Red redobj = new Red();
         Orange orangeobj = new Orange();
-        redobj.show();
-        orangeobj.show();
+        redobj.showMyFavoriteColorOfAllTimefromTheRainbow();
+        orangeobj.showMyFavoriteColorOfAllTimefromTheRainbow();
 
     }
 
