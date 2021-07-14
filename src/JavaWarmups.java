ce90aaa0d7e7cd94ba495634a84db701203143be
Amado Azua III <amado.azua3@gmail.com>
2021-07-14 09:28:01 -0500

    Java warmup

---
+++ src/JavaWarmups.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class JavaWarmups {
+
+    public static void main(String[] args) {
+
+        String strExample = "A B C D E F G";
+
+        String strExp = "Erik Moses Chris Amado CyberPunk2077";
+
+        System.out.println(stringy(strExample));
+        System.out.println(stringy(strExp));
+
+    }
+
+    public static String stringy(String str){
+
+        String[] splitty = str.split(" ");
+
+        return Arrays.toString(splitty);
+
+    }
+
+}
